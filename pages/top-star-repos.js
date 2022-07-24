699b21d45ab83db446e630e6b9186fb7e2677fa0
siddontang <siddontang@gmail.com>
2022-07-24 00:35:43 -0700

    first commit
    Signed-off-by: siddontang <siddontang@gmail.com>

---
+++ pages/top-star-repos.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearScale, BarElement } from 'chart.js'
+Chart.register(CategoryScale, LinearScale, BarElement)
+
+
+function buildData(rows) {
+    const labels = rows.map(row => row.repo_name)
+    const data = rows.map(row => row.events_count)
+    const backgroundColor = rows.map(row => 'rgba(255, 99, 132, 0.2)')
+    const borderColor = rows.map(row => 'rgba(255, 99, 132, 1)')
+    return {
+        labels,
+        datasets: [{
+            label: '# of Top Stars',
+            data,
+            backgroundColor,
+            borderColor,
+            borderWidth: 1
+        }]
+    }
+}
+
+export default function TopStarRepos({ rows }) {
+    const data = buildData(rows)
+
+    return {
+        displayName: 'TopStarRepos',
+        render() {
+            return (
+                <div>
+                    <h2>Top Star Repos</h2>
+                    <Bar
+                        data={data}
+                        width={800}
+                        height={400}
+                        options={{
+                            responsive: false,
+                            maintainAspectRatio: false
+                        }}
+                    />
+                </div>
+            );
+        }
+    }
+}
+
+
