73aa4dec98da9b58356a43f029f426fccce790e4
siddontang <siddontang@gmail.com>
2022-07-24 10:45:22 -0700

    remove dark mode
    Signed-off-by: siddontang <siddontang@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import TopStarRepos from './top-star-repos'
 import executeQuery from '../lib/tidb'
 
